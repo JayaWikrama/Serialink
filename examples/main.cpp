8f0249d69b010402ed74295a0c987ecf1d5f78dd
JayaWikrama <jayawikrama89@gmail.com>
2024-07-22 20:20:26 +0700

    Menambah example untuk beberapa fungsi/method tambahan.

--- examples/main.cpp
+++ examples/main.cpp
@@ -11,13 +11,37 @@ int main(int argc, char **argv){
     Serial serial(argv[1], B115200, atoi(argv[2]));
     serial.openPort();
     serial.setKeepAlive(atoi(argv[3]));
-    while (serial.readData()){
+    /* test start bytes */
+    while (serial.readStartBytes((const unsigned char *) "\x31\x32\x33", 3)){
         std::cout << "timeout!!!" << std::endl;
     }
     char data[512];
     memset(data, 0x00, sizeof(data));
     serial.getBuffer((unsigned char *) data, sizeof(data));
-    std::cout << data << std::endl;
+    std::cout << "Data: " << data << std::endl;
+    memset(data, 0x00, sizeof(data));
+    serial.getRemainingBuffer((unsigned char *) data, sizeof(data));
+    std::cout << "Remaining Data: " << data << std::endl;
+    /* test N Bytes Length */
+    while (serial.readNBytes(24)){
+        std::cout << "timeout!!!" << std::endl;
+    }
+    memset(data, 0x00, sizeof(data));
+    serial.getBuffer((unsigned char *) data, sizeof(data));
+    std::cout << "Data: " << data << std::endl;
+    memset(data, 0x00, sizeof(data));
+    serial.getRemainingBuffer((unsigned char *) data, sizeof(data));
+    std::cout << "Remaining Data: " << data << std::endl;
+    /* test Read Until Stop Bytes */
+    while (serial.readUntilStopBytes((const unsigned char *) "\x31\x32\x33\x34", 4)){
+        std::cout << "timeout!!!" << std::endl;
+    }
+    memset(data, 0x00, sizeof(data));
+    serial.getBuffer((unsigned char *) data, sizeof(data));
+    std::cout << "Data: " << data << std::endl;
+    memset(data, 0x00, sizeof(data));
+    serial.getRemainingBuffer((unsigned char *) data, sizeof(data));
+    std::cout << "Remaining Data: " << data << std::endl;
     serial.closePort();
     return 0;
 }
