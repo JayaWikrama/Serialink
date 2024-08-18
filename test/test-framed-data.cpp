77de11f84774dd6f71b6aad14c3978d577b11d05
JayaWikrama <jayawikrama89@gmail.com>
2024-08-18 15:37:49 +0700

    test/add: pembacaan framed data untuk jumlah data yang tidak diketahui sebelum stop bytes.

--- test/test-framed-data.cpp
+++ test/test-framed-data.cpp
@@ -360,3 +360,99 @@ TEST_F(SerialinkFramedDataTest, ReadTest_withPrefixAndSuffix_3) {
     ASSERT_EQ(slave.getRemainingBuffer(tmp), 0);
     ASSERT_EQ(tmp.size(), 0);
 }
+
+TEST_F(SerialinkFramedDataTest, ReadTest_withUnknownDataSz_1) {
+    unsigned char buffer[64];
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    std::vector <unsigned char> tmp;
+    slave.setPort(master.getVirtualPortName());
+    slave.setBaudrate(B115200);
+    slave.setTimeout(25);
+    slave.setKeepAlive(1000);
+    DataFrame startBytes(DataFrame::FRAME_TYPE_START_BYTES, "1234");
+    DataFrame dataBytes(DataFrame::FRAME_TYPE_DATA);
+    DataFrame stopBytes(DataFrame::FRAME_TYPE_STOP_BYTES, "90-=");
+    slave = startBytes + dataBytes + stopBytes;
+    ASSERT_EQ(slave.getFormat()->getDataFrameFormat(),
+              "FRAME_TYPE_START_BYTES[size:4]:<<31323334>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_DATA[size:0]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_STOP_BYTES[size:4]:<<39302D3D>><<exeFunc:0>><<postFunc:0>>\n");
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(slave.openPort(), 0);
+    ASSERT_EQ(slave.writeData("m1234567890-=qwertyuiopplkjhgfdsaZxcvbh76redcvbnm,mvdswertyuioiuhgfcxvbnm"), 0);
+    ASSERT_EQ(master.begin(), true);
+    ASSERT_EQ(slave.readFramedData(), 0);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 75, true);
+    ASSERT_EQ(slave.getDataSize(), 12);
+    ASSERT_EQ(slave.getBuffer(buffer, sizeof(buffer)), 12);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "1234567890-=", 12), 0);
+    ASSERT_EQ(slave.getBuffer(tmp), 12);
+    ASSERT_EQ(tmp.size(), 12);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "1234567890-=", 12), 0);
+    ASSERT_EQ(slave.getRemainingDataSize(), 60);
+    ASSERT_EQ(slave.getRemainingBuffer(buffer, sizeof(buffer)), 60);
+    ASSERT_EQ(memcmp(buffer, (unsigned char *) "qwertyuiopplkjhgfdsaZxcvbh76redcvbnm,mvdswertyuioiuhgfcxvbnm", 60), 0);
+    ASSERT_EQ(slave.getRemainingBuffer(tmp), 60);
+    ASSERT_EQ(tmp.size(), 60);
+    ASSERT_EQ(memcmp(tmp.data(), (unsigned char *) "qwertyuiopplkjhgfdsaZxcvbh76redcvbnm,mvdswertyuioiuhgfcxvbnm", 60), 0);
+}
+
+TEST_F(SerialinkFramedDataTest, ReadTest_withUnknownDataSz_2) {
+    unsigned char buffer[16];
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    int i = 0;
+    std::vector <unsigned char> tmp;
+    slave.setPort(master.getVirtualPortName());
+    slave.setBaudrate(B115200);
+    slave.setTimeout(25);
+    slave.setKeepAlive(1000);
+    DataFrame startBytes(DataFrame::FRAME_TYPE_START_BYTES, "1234");
+    DataFrame dataBytes(DataFrame::FRAME_TYPE_DATA);
+    DataFrame stopBytes(DataFrame::FRAME_TYPE_STOP_BYTES, "90-=");
+    slave = startBytes + dataBytes + stopBytes;
+    ASSERT_EQ(slave.getFormat()->getDataFrameFormat(),
+              "FRAME_TYPE_START_BYTES[size:4]:<<31323334>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_DATA[size:0]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_STOP_BYTES[size:4]:<<39302D3D>><<exeFunc:0>><<postFunc:0>>\n");
+    ASSERT_EQ(slave.openPort(), 0);
+    ASSERT_EQ(slave.writeData("qwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnm"), 0);
+    ASSERT_EQ(master.begin(), true);
+    for (i = 0; i < 4; i++){
+        gettimeofday(&tvStart, NULL);
+        ASSERT_EQ(slave.readFramedData(), 0);
+        gettimeofday(&tvEnd, NULL);
+        diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+        ASSERT_EQ(diffTime == 0, true);
+        ASSERT_EQ(slave.getDataSize(), 12);
+        ASSERT_EQ(slave.getBuffer(buffer, sizeof(buffer)), 12);
+        ASSERT_EQ(memcmp(buffer, (const unsigned char *) "1234567890-=", 12), 0);
+        ASSERT_EQ(slave.getBuffer(tmp), 12);
+        ASSERT_EQ(tmp.size(), 12);
+        ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "1234567890-=", 12), 0);
+    }
+    ASSERT_EQ(slave.getRemainingDataSize(), 7);
+    ASSERT_EQ(slave.getRemainingBuffer(buffer, sizeof(buffer)), 7);
+    ASSERT_EQ(memcmp(buffer, (unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(slave.getRemainingBuffer(tmp), 7);
+    ASSERT_EQ(tmp.size(), 7);
+    ASSERT_EQ(memcmp(tmp.data(), (unsigned char *) "zxcvbnm", 7), 0);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(slave.readFramedData(), 2);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 2400 && diffTime <= 2800, true);
+    ASSERT_EQ(slave.getDataSize(), 7);
+    ASSERT_EQ(slave.getBuffer(buffer, sizeof(buffer)), 7);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(slave.getBuffer(tmp), 7);
+    ASSERT_EQ(tmp.size(), 7);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(slave.getRemainingDataSize(), 0);
+    ASSERT_EQ(slave.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(slave.getRemainingBuffer(tmp), 0);
+    ASSERT_EQ(tmp.size(), 0);
+}
